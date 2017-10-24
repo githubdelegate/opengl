69a4feba868e3aed05f12a77e1f844fb53fdc4fd
zy <zy>
2017-10-24 12:46:56 +0800

    add detail comment

--- esgl_1/texture/ZYTextureView.m
+++ esgl_1/texture/ZYTextureView.m
@@ -26,75 +26,30 @@
 @end
 
 @implementation ZYTextureView
-+ (Class)layerClass{
-    return [CAEAGLLayer class];
-}
-
-- (instancetype)initWithFrame:(CGRect)frame{
-    
-    if (self = [super initWithFrame:frame]) {
-        [self settingDrawable];
-        [self settingDefault];
-    }
-    return self;
-}
-
-
-- (void)settingDrawable {
-    
-    CAEAGLLayer *layer = (CAEAGLLayer *)self.layer;
-    layer.drawableProperties = @{kEAGLDrawablePropertyColorFormat : kEAGLColorFormatRGBA8,
-                                 kEAGLDrawablePropertyRetainedBacking : @(YES)};
-    layer.contentsScale = [UIScreen mainScreen].scale;
-    layer.opaque = YES;
-}
-
-- (void)settingDefault {
-    self.currentTransforms = self.oldTransform = [[VYTransforms alloc] init];
-    [self setDefaultTransforms];
-}
-
-- (void)setDefaultTransforms {
-    VYTransforms *trans = self.currentTransforms;
-    // Model Transform
-    trans.modelTransform = VYSTTransformMake(trans.PositionVec3Make(0, 0, 0),
-                                             trans.RotationVec3Make(0, 0, 0),
-                                             trans.ScalingVec3Make(1, 1, 1));
-    // View Transform
-    trans.viewTransform = VYSTTransformMake(trans.PositionVec3Make(0, 0, 0),
-                                            trans.RotationVec3Make(0, 0, 0),
-                                            trans.ScalingVec3Make(1, 1, 1));
-    // Projection Transform
-    // LookAt
-    trans.lookAt = VYLookAtMake(trans.EyeVec3Make(0, 0, 0),
-                                trans.CenterVec3Make(0, 0, -1),
-                                trans.UpVec3Make(0, 1, 0));
-    trans.aspectRadio = (self.bounds.size.width / self.bounds.size.height);
-    trans.perspectiveProj = VYPerspectivePerspectiveMake(GLKMathDegreesToRadians(85.0),
-                                                         trans.aspectRadio,
-                                                         1, 150);
-    
-}
 
 - (void)layoutSubviews{
     
     self.ctx = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
     [EAGLContext setCurrentContext:self.ctx];
     
+    //------ 1. 配置帧，渲染缓存 ---------
+    // 1.1 帧缓存
     glGenFramebuffers(1, &fboId);
     glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+    // 1.2 渲染缓存
     glGenRenderbuffers(1, &colorRboId);
     glBindRenderbuffer(GL_RENDERBUFFER, colorRboId);
     [self.ctx renderbufferStorage:GL_RENDERBUFFER fromDrawable:(CAEAGLLayer *)self.layer];
+    
+    // 1.3 绑定渲染缓存
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorRboId);
     
-    // Depth Render
+    // 1.4 计算渲染宽高
     GLint renderWidth = 0, renderHeight = 0;
     glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &renderWidth);
     glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &renderHeight);
     
-    
-    //--准备数据
+    //----- 2. 准备顶点和下标数据 -----
     glGenBuffers(1, &vboId);
     glBindBuffer(GL_ARRAY_BUFFER, vboId);
     const GLvoid *data = NULL;
@@ -104,40 +59,26 @@
     indicts = squareIndicts;
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(squareIndicts), indicts, GL_STATIC_DRAW);
     
-    // shader
+    //------- 3. 设置shader program -------
     GLuint vShaderId = [self addShader:GL_VERTEX_SHADER];
     GLuint fShaderId = [self addShader:GL_FRAGMENT_SHADER];
     programId = glCreateProgram();
-    
     glAttachShader(programId, vShaderId);
     glAttachShader(programId, fShaderId);
-    
     glBindAttribLocation(programId, 0, "a_position");
     glBindAttribLocation(programId, 1, "a_texCoord");
-    
     glLinkProgram(programId);
     
-    // 给shader中参数赋值
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(ZYVextex), (const GLvoid*)offsetof(ZYVextex, postion));
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(ZYVextex), (const GLvoid *)offsetof(ZYVextex, texCoord));
-    
-    // texture
-    glGenTextures(1, &textureId);
-    glBindTexture(GL_TEXTURE_2D, textureId);
-    
-    
-    // 开始渲染
-    // 设置背景
+    //------ 4.使用shader程序 ------
+    // 4.1 设置背景
     glClearColor(0.3,0.3, 0.3, 1);
+    // 4.2 清理缓存
     glClear(GL_COLOR_BUFFER_BIT);
+    // 4.3 设置视口
     glViewport(0, 0, renderWidth, renderHeight);
-    
-    // 加载数据
+    // 4.4 使用shader 程序
     glUseProgram(programId);
-    
-    // 坐标转换
+    // 4.5 坐标转换 计算变换矩阵
     GLuint modelViewLoc = glGetUniformLocation(programId, "u_modelViewMat4");
     GLuint projectionLoc = glGetUniformLocation(programId, "u_projectionMat4");
     
@@ -151,27 +92,38 @@
     trans.baseCameraMat4 = VYCameraMat4Make(trans.baseCamera);
     trans.mvpTransfrom = VYMVPTransformMake(trans.modelTransformMat4, trans.viewTransformMat4, trans.baseCameraMat4);
     
+    // 4.6 给shader中uniform变量赋值
     VYMVPTransform mvp = trans.mvpTransfrom;
     glUniformMatrix4fv(modelViewLoc, 1, GL_FALSE, VYMVPTransformModelViewMat4Make(mvp).m);
     glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, mvp.cameraProjMat4.m);
+
+    // 4.7 给attribute变量赋值
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(ZYVextex), (const GLvoid*)offsetof(ZYVextex, postion));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(ZYVextex), (const GLvoid *)offsetof(ZYVextex, texCoord));
     
-    // 加载纹理数据
+    //------ 5. 创建texture -----
+    glGenTextures(1, &textureId);
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    glEnable(GL_TEXTURE_2D);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    // 5.1给sample变量赋值
+    GLuint textureSourceLoc = glGetUniformLocation(programId, "us2d_texture");
+    glUniform1i(textureSourceLoc, 0);
+    
+    // 5.2 加载纹理数据
     [self p_loadTextureImg:@"512_512" completion:^(NSMutableData *data, size_t newWidth, size_t newHeight) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, newWidth, newHeight, GL_FALSE, GL_RGBA, GL_UNSIGNED_BYTE, data.bytes);
     }];
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     
-    GLuint textureSourceLoc = glGetUniformLocation(programId, "us2d_texture");
-    glUniform1i(textureSourceLoc, 0);
-    
-    glEnable(GL_TEXTURE_2D);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, textureId);
-    
-    // 绘制
+    //----- 6.绘制------
     glBindRenderbuffer(GL_RENDERBUFFER, colorRboId);
     glDrawElements(GL_TRIANGLES, sizeof(indicts)/sizeof(indicts[0]), GL_UNSIGNED_BYTE, indicts);
+//    glDrawArrays(GL_TRIANGLES, 0, 4);
     [self.ctx presentRenderbuffer:GL_RENDERBUFFER];
  
     // delete source
@@ -277,4 +229,55 @@
         block(imageData,width,height);
     }
 }
+
++ (Class)layerClass{
+    return [CAEAGLLayer class];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    
+    if (self = [super initWithFrame:frame]) {
+        [self settingDrawable];
+        [self settingDefault];
+    }
+    return self;
+}
+
+
+- (void)settingDrawable {
+    
+    CAEAGLLayer *layer = (CAEAGLLayer *)self.layer;
+    layer.drawableProperties = @{kEAGLDrawablePropertyColorFormat : kEAGLColorFormatRGBA8,
+                                 kEAGLDrawablePropertyRetainedBacking : @(YES)};
+    layer.contentsScale = [UIScreen mainScreen].scale;
+    layer.opaque = YES;
+}
+
+- (void)settingDefault {
+    self.currentTransforms = self.oldTransform = [[VYTransforms alloc] init];
+    [self setDefaultTransforms];
+}
+
+- (void)setDefaultTransforms {
+    VYTransforms *trans = self.currentTransforms;
+    // Model Transform
+    trans.modelTransform = VYSTTransformMake(trans.PositionVec3Make(0, 0, 0),
+                                             trans.RotationVec3Make(0, 0, 0),
+                                             trans.ScalingVec3Make(1, 1, 1));
+    // View Transform
+    trans.viewTransform = VYSTTransformMake(trans.PositionVec3Make(0, 0, 0),
+                                            trans.RotationVec3Make(0, 0, 0),
+                                            trans.ScalingVec3Make(1, 1, 1));
+    // Projection Transform
+    // LookAt
+    trans.lookAt = VYLookAtMake(trans.EyeVec3Make(0, 0, 0),
+                                trans.CenterVec3Make(0, 0, -1),
+                                trans.UpVec3Make(0, 1, 0));
+    trans.aspectRadio = (self.bounds.size.width / self.bounds.size.height);
+    trans.perspectiveProj = VYPerspectivePerspectiveMake(GLKMathDegreesToRadians(85.0),
+                                                         trans.aspectRadio,
+                                                         1, 150);
+    
+}
+
 @end
